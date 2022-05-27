0a1cbe40ea757eb09484dc036a21807471dd20a3
shah726 <mdsh9206@gmail.com>
2022-05-27 10:15:56 +0530

    third Changes

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
 import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import axios from "axios";
+import https from "https";
 
-export default function Home() {
-  const [data1, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("https://alaqsafunduat-api.isdb.org/static-stites?_limit=-1")
-      // .then((res) => res.json())
-      .then((res) => {
-        console.log(res.data[0].static.about_introduction);
-        setData(res.data);
-      });
-  }, []);
+export default function Home({ posts }) {
+  // const [data1, setData] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://alaqsafunduat-api.isdb.org/static-stites?_limit=-1")
+  //     // .then((res) => res.json())
+  //     .then((res) => {
+  //       console.log(res.data[0].static.about_introduction);
+  //       setData(res.data);
+  //     });
+  // }, []);
+
+  // axios.defaults.httpsAgent = new https.Agent({
+  //   rejectUnauthorized: false,
+  // });
 
   return (
     <div className={styles.container}>
       <h1>Shahid</h1>
       <div>
-        {data1.map((dat) => (
+        {posts.map((dat) => (
           <div key={dat.id}>
             <p>{dat.id}</p>
             <p>{dat.locale}</p>
@@ -29,4 +34,22 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const res = await axios.get(
+    "https://alaqsafunduat-api.isdb.org/static-stites?_limit=-1",
+    {
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    }
+  );
+  console.log(res.data);
+
+  return {
+    props: {
+      posts: res.data,
+    },
+  };
 }
